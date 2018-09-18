34a2ebb49a23c149a43d64cd6372dadc48a43da3
David Rosenblum <david@rosenblum.net>
2018-09-17 22:35:49 -0400

    Initial files

---
+++ Avocado.py
@@ -0,0 +1,61 @@
+import json
+
+# Object to hold avocado data
+# Static method to read the data set
+# https://www.kaggle.com/neuromusic/avocado-prices
+
+class Avocado:
+    def __init__(self, date, avg_price, avocado_type, year, region, total_volume, plu4046, plu4225, plu4770):
+        # observation date
+        self.date = date
+        # average price of a single avocado
+        self.avg_price = avg_price
+        # conventional or organic
+        self.avocado_type = avocado_type
+        # the year
+        self.year = year
+        # city or region of the observation
+        self.region = region
+        # total number of avocados sold
+        self.total_volume_sold = total_volume
+        # total number of avocados with PLU 4046 (small hass) sold
+        self.plu_4046_sold = plu4046
+        # total number of avocados with PLU 4225 (large hass) sold
+        self.plu_4225_sold = plu4225
+        # total number of avocados with PLU 4770 (extra large hass) sold
+        self.plu_4770_sold = plu4770
+
+    def __str__(self):
+        return json.dumps(self.__dict__)
+
+    @staticmethod
+    # parses the avocado csv data file
+    async def read_data_set():
+        rows = []
+        file = open("data/avocado.csv")
+
+        schema_line = True
+
+        for line in file:
+            if schema_line:
+                schema_line = False
+                continue
+
+            cols = line.split(",")
+
+            # extract required columns to create the avocado object
+            rows.append(Avocado(
+                cols[1],
+                float(cols[2]),
+                cols[11],
+                int(cols[12]),
+                cols[13].replace("\n", ""),
+                float(cols[3]),
+                float(cols[4]),
+                float(cols[5]),
+                float(cols[6])
+            ))
+
+        file.close()
+
+        return rows
---
+++ main.py
@@ -0,0 +1,58 @@
+import asyncio
+import json
+from Avocado import Avocado
+
+
+def build_model(data_set):
+    model = {}
+
+    for row in data_set:
+        # create year if it does not exist
+        if row.year not in model:
+            model[row.year] = {}
+
+        # create region if it does not exist in the year
+        if row.region not in model[row.year]:
+            model[row.year][row.region] = {}
+
+        # create avocado type if it does not exist in the year's region
+        # initialize with 0 for numerical values that are updated later
+        if row.avocado_type not in model[row.year][row.region]:
+            model[row.year][row.region][row.avocado_type] = {
+                'total': 0, '4046': 0, '4225': 0, '4770': 0, 'avg_price': 0, "count": 0
+            }
+
+        # update volume, price, frequency data
+        model[row.year][row.region][row.avocado_type]["total"] += round(row.total_volume_sold)
+        model[row.year][row.region][row.avocado_type]["4046"] += round(row.plu_4046_sold)
+        model[row.year][row.region][row.avocado_type]["4225"] += round(row.plu_4225_sold)
+        model[row.year][row.region][row.avocado_type]["4770"] += round(row.plu_4770_sold)
+        model[row.year][row.region][row.avocado_type]["avg_price"] += row.avg_price
+        model[row.year][row.region][row.avocado_type]["count"] += 1
+
+    # for each group... calculate averages
+    for year in model:
+        for region in model[year]:
+            for a_type in model[year][region]:
+                group = model[year][region][a_type]
+                group["avg_price"] = round(group["avg_price"] / group["count"], 2)
+
+    return model
+
+
+# main method
+# loads the data (async) and creates the model
+async def main():
+    # load the data set
+    data_set = await Avocado.read_data_set()
+
+    # construct the model with the data set
+    model = build_model(data_set)
+
+    # print the model
+    print(json.dumps(model, indent="\t"))
+
+
+# call main
+if __name__ == "__main__":
+    asyncio.run(main())
